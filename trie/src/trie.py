7843a537cbf846cd3bc2db73bc0bf6f23c685bd5
Fahad Naaz <fahadnaaz@Fahads-MacBook-Pro.local>
2022-12-24 15:29:58 +0530

    added files for trie package

---
+++ trie/src/trie.py
@@ -0,0 +1,29 @@
+from entities import Node
+
+class Trie:
+    def __init__(self) -> None:
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = Node()
+            curr = curr.children[c]
+        curr.end_of_word = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.end_of_word
+        
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
